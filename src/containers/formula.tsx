4833c192408a73c63daca0d6d8865044996b0714
Clupai8o0 <clupaio4@gmail.com>
2022-08-14 19:38:32 +0300

    basic setup for decimal to binary conversion formula

--- src/containers/formula.tsx
+++ src/containers/formula.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// State Management
+import { useRecoilValue } from "recoil";
+import { fromTypeState, toTypeState } from "../atoms/typesAtom";
+
+// Formula Components
+import DecimalToBinary from "../components/formulas/decimal-to-binary";
+
+// Types
+import { Types } from "../lib/types";
 
 const Formula = () => {
+  const fromType = useRecoilValue(fromTypeState);
+  const toType = useRecoilValue(toTypeState);
+
   return (
-    <article>Formula</article>
-  )
+		<article>
+			<h1>Formula</h1>
+
+			<section>
+				{fromType === Types.Decimal && toType === Types.Binary ? <DecimalToBinary /> : ""}
+			</section>
+		</article>
+	);
 }
 
 export default Formula
