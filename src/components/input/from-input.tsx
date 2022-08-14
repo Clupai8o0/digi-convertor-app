c6eaa29edc1f0cdb18c91efb2adf55ae62018411
Clupai8o0 <clupaio4@gmail.com>
2022-08-14 08:55:16 +0300

    added validation for right values to be inserted

--- src/components/input/from-input.tsx
+++ src/components/input/from-input.tsx
@@ -1,7 +1,38 @@
+// Imports
+import { useState } from "react";
+
+// State Management
+import { useRecoilState } from "recoil";
+import { fromTypeState } from "../../atoms/typesAtom";
+import { getProbables } from "../../lib/types";
+
+// Styles
 import styles from "./input.module.css";
 
 const Input = () => {
-  return (
+	const [value, setValue] = useState("");
+	const [fromType, setFromType] = useRecoilState(fromTypeState);
+
+	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+		const inputArr = event.target.value.split("");
+		const newArr = [];
+
+		getProbables(fromType).forEach((probable) => {
+			for (let i = 0; i < inputArr.length; i++) {
+				if (inputArr[i] === probable) {
+					newArr.push(probable);
+					inputArr.splice(i, 1);
+					i = -1;
+				}
+			}
+		});
+
+		if (inputArr.length === 0) {
+			setValue(event.target.value);
+		}
+	}
+
+	return (
 		<div className={styles.inputGroup}>
 			<input
 				required
@@ -9,10 +40,12 @@ const Input = () => {
 				name="text"
 				autoComplete="off"
 				className={styles.input}
+				value={value}
+				onChange={(e) => handleInputChange(e)}
 			/>
 			<label className={styles.userLabel}>Binary</label>
 		</div>
 	);
-}
+};
 
-export default Input
+export default Input;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -17,6 +17,35 @@ export function getTypeBase(type: Types): String {
 	}
 }
 
+export function getProbables(type: Types): String[] {
+	if (type === Types.Binary) {
+		return ["0", "1"]
+	} else if (type === Types.Decimal) {
+		return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	} else if (type === Types.Octal) {
+		return ["0", "1", "2", "3", "4", "5", "6", "7"];
+	} else {
+		return [
+			"0",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"a",
+			"b",
+			"c",
+			"d",
+			"e",
+			"f",
+		];
+	}
+}
+
 export enum Theme {
 	Light = "Light",
 	Dark = "Dark",
