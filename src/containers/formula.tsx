d6c78be3cf2e9404ad2348e7054de860e0493e5f
Clupai8o0 <clupaio4@gmail.com>
2022-08-17 11:18:33 +0300

    refactored formula code

--- src/containers/formula.tsx
+++ src/containers/formula.tsx
@@ -3,98 +3,70 @@ import { useRecoilValue } from "recoil";
 import { fromTypeState, toTypeState } from "../atoms/typesAtom";
 
 // Formula Components
-import DecimalToBinary from "../components/formulas/decimal-to-binary";
 import BinaryToDecimal from "../components/formulas/binary-decimal";
 import BinaryToOctal from "../components/formulas/binary-octal";
+import BinaryToHex from "../components/formulas/binary-hex";
+
+import DecimalToBinary from "../components/formulas/decimal-to-binary";
+import DecimalToHex from "../components/formulas/decimal-hex";
+import DecimalToOctal from "../components/formulas/decimal-octal";
+
+import OctalToDecimal from "../components/formulas/octal-decimal";
+import OctalToBinary from "../components/formulas/octal-binary";
+
+import HexToDecimal from "../components/formulas/hex-decimal";
+import HexToBinary from "../components/formulas/hex-binary";
 
 // Types
 import { Types } from "../lib/types";
-import OctalToDecimal from "../components/formulas/octal-decimal";
-import DecimalToHex from "../components/formulas/decimal-hex";
-import DecimalToOctal from "../components/formulas/decimal-octal";
-import OctalToBinary from "../components/formulas/octal-binary";
-import HexToDecimal from "../components/formulas/hex-decimal";
-import BinaryToHex from "../components/formulas/binary-hex";
-import HexToBinary from "../components/formulas/hex-binary";
 
 const Formula = () => {
 	const fromType = useRecoilValue(fromTypeState);
 	const toType = useRecoilValue(toTypeState);
+
+	function getFormula() {
+		if (fromType === Types.Binary) {
+			if (toType === Types.Decimal) return <BinaryToDecimal />;
+			if (toType === Types.Octal) return <BinaryToOctal />;
+			if (toType === Types.Hexadecimal) return <BinaryToHex />;
+		}
+
+		if (fromType === Types.Decimal) {
+			if (toType === Types.Binary) return <DecimalToBinary />;
+			if (toType === Types.Octal) return <DecimalToOctal />;
+			if (toType === Types.Hexadecimal) return <DecimalToHex />;
+		}
+
+		if (fromType === Types.Octal) {
+			if (toType === Types.Binary) return <OctalToBinary />;
+			if (toType === Types.Decimal) return <OctalToDecimal />;
+			if (toType === Types.Hexadecimal) return (
+				<>
+					<OctalToBinary />
+					<BinaryToHex />
+				</>
+			)
+		}
+
+		if (fromType === Types.Hexadecimal) {
+			if (toType === Types.Binary) return <HexToBinary />;
+			if (toType === Types.Decimal) return <HexToDecimal />;
+			if (toType === Types.Octal)
+				return (
+					<>
+						<HexToBinary />
+						<BinaryToOctal />
+					</>
+				);
+		}
+	}
 
 	return (
 		<article>
 			<h1>Formula</h1>
 
 			<section className="formula">
-				{fromType === Types.Binary && toType === Types.Decimal ? (
-					<BinaryToDecimal />
-				) : (
-					""
-				)}
-				{fromType === Types.Binary && toType === Types.Octal ? (
-					<BinaryToOctal />
-				) : (
-					""
-				)}
-				{fromType === Types.Binary && toType === Types.Hexadecimal ? (
-					<BinaryToHex />
-				) : (
-					""
-				)}
-
-				{fromType === Types.Decimal && toType === Types.Binary ? (
-					<DecimalToBinary />
-				) : (
-					""
-				)}
-				{fromType === Types.Decimal && toType === Types.Hexadecimal ? (
-					<DecimalToHex />
-				) : (
-					""
-				)}
-				{fromType === Types.Decimal && toType === Types.Octal ? (
-					<DecimalToOctal />
-				) : (
-					""
-				)}
-
-				{fromType === Types.Octal && toType === Types.Decimal ? (
-					<OctalToDecimal />
-				) : (
-					""
-				)}
-				{fromType === Types.Octal && toType === Types.Binary ? (
-					<OctalToBinary />
-				) : (
-					""
-				)}
-				{fromType === Types.Octal && toType === Types.Hexadecimal ? (
-					<>
-						<OctalToBinary />
-						<BinaryToHex />
-					</>
-				) : (
-					""
-				)}
-
-				{fromType === Types.Hexadecimal && toType === Types.Binary ? (
-					<HexToBinary />
-				) : (
-					""
-				)}
-				{fromType === Types.Hexadecimal && toType === Types.Decimal ? (
-					<HexToDecimal />
-				) : (
-					""
-				)}
-				{fromType === Types.Hexadecimal && toType === Types.Octal ? (
-					<>
-						<HexToBinary />
-						<BinaryToOctal />
-					</>
-				) : (
-					""
-				)}
+				{getFormula()}
 			</section>
 		</article>
 	);
