2fd01927d305ff6fc281d9c2f61de0d8ba5f31d4
Clupai8o0 <clupaio4@gmail.com>
2022-08-18 16:08:19 +0300

    scrolling to top on load

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import "./media-queries.css";
 
 import { RecoilRoot } from "recoil";
 
+window.scrollTo({
+	top: 0,
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<RecoilRoot>
